399cb634f9d90379111cae73a53fa43d685ae996
Glitch (glitch-hello-react) <none>
2022-06-20 09:22:47 +0000

    🎭🎥 Checkpoint ./emprator.js:35490524/1306 ./package.json:35490524/992

--- emprator.js
+++ emprator.js
@@ -1,13 +1,4 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-app.listen(3000, () => {
-  console.log('Server Started.');
-});
 const emprator = new Discord.Client();
 const emprator2 = new Discord.Client();
 const emprator3 = new Discord.Client();
@@ -16,24 +7,7 @@ const emprator5 = new Discord.Client();
 ////////////
 const prefix = "!!";
 ////////////
-emprator.on('ready', () => {
-	console.log(`${client.user.tag}`);
-	console.log(`${client.guilds.cache.size} Servers`);
-	console.log(`${client.users.cache.size} Members`);
-	console.log(`${client.channels.cache.size} Channels`);
-	console.log(`[ ${client.guilds.cache.map(g => g.name).join(', \n ')} ]`);
-	client.user.setActivity(`emprator`, { type: 'PLAYING' });
-});
-emprator2.on('ready', () => {
-	console.log(`${client.user.tag}`);
-	console.log(`${client.guilds.cache.size} Servers`);
-	console.log(`${client.users.cache.size} Members`);
-	console.log(`${client.channels.cache.size} Channels`);
-	console.log(`[ ${client.guilds.cache.map(g => g.name).join(', \n ')} ]`);
-	client.user.setActivity(`emprator`, { type: 'PLAYING' });
-});
-/////////////
-emprator.login("OTg4MzY3MDMxMDU1Mjk4NTYw.G2nhfp.c25ligZCInTqXmq9eNb6bcvQm1XJUriBaUfWdM");
+emprator.login("OTg4MzcwMDAwMzIwNTQ0NzY5.GkAPVb.dTTTd5Ss3GLIg17GRply_sGNnL6Sb-HN8abmVo");
 emprator.on("message", message => {
   if (message.channel.id === "842532039874052106") {
     message.react("🤍").then(() => {
@@ -42,7 +16,7 @@ emprator.on("message", message => {
   }
 });
 //////////
-emprator2.login("ODY2NzAzMzc4MTgwMzQxODAz.GNE9I4.4KMhJt-RSygdgbmZKMMjLeUXWoQwevVvABc_0o");
+emprator2.login("ODY2NzAzMzc4MTgwMzQxODAz.GNE9I4.4KMhJt-RSybmZKMMjLeUXWoQwevVvABc_0o");
 emprator2.on("message", message => {
   if (message.channel.id === "842532039874052106") {
     message.react("🤍").then(() => {
