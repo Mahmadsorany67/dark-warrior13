eacabe47813cde787738df8c15a7bc4a73dcb1a6
Glitch (glitch-hello-react) <none>
2022-06-20 10:40:43 +0000

    🍬📹 Checkpoint ./emprator.js:35490524/5191

--- emprator.js
+++ emprator.js
@@ -4,21 +4,47 @@ const emprator2 = new Discord.Client();
 const emprator3 = new Discord.Client();
 const emprator4 = new Discord.Client();
 const emprator5 = new Discord.Client();
+const emprator6 = new Discord.Client();
+const emprator7 = new Discord.Client();
+const emprator8 = new Discord.Client();
+const emprator9 = new Discord.Client();
+const emprator10 = new Discord.Client();
+const emprator11 = new Discord.Client();
+const emprator12 = new Discord.Client();
+const emprator13 = new Discord.Client();
+const emprator14 = new Discord.Client();
+const emprator15 = new Discord.Client();
+const emprator16 = new Discord.Client();
+const emprator17 = new Discord.Client();
+const emprator18 = new Discord.Client();
+const emprator19 = new Discord.Client();
+const emprator20 = new Discord.Client();
+const emprator21 = new Discord.Client();
+const emprator22 = new Discord.Client();
+const emprator23 = new Discord.Client();
+const emprator24 = new Discord.Client();
+const emprator25 = new Discord.Client();
+
+
+
+
+
+
 ////////////
-const prefix = "!!";
+const prefix = "emprator";
 ////////////
-emprator.login("OTg4MzcwMDAwMzIwNTQ0NzY5.GkAPVb.dTTTd5Ss3GLIg17GRply_sGNnL6Sb-HN8abmVo");
+emprator.login("");
 emprator.on("message", message => {
-  if (message.channel.id === "842532039874052106") {
+  if (message.channel.id === "NONE") {
     message.react("🤍").then(() => {
       message.react("🖤");
     });
   }
 });
 //////////
-emprator2.login("ODY2NzAzMzc4MTgwMzQxODAz.GNE9I4.4KMhJt-RSybmZKMMjLeUXWoQwevVvABc_0o");
+emprator2.login("");
 emprator2.on("message", message => {
-  if (message.channel.id === "842532039874052106") {
+  if (message.channel.id === "NONE") {
     message.react("🤍").then(() => {
       message.react("🖤");
     });
@@ -51,3 +77,191 @@ emprator5.on("message", message => {
     });
   }
 });
+////////
+emprator6.login("");
+emprator6.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////
+emprator7.login("");
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////
+emprator8.login("");
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+//////
+emprator9.login("");
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////////
+emprator10.login("");
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////
+emprator11.login("");
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+/////////
+emprator12.login("");
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+/////////
+emprator13.login("");
+client.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+//////
+emprator14.login("");
+emprator14.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+////////
+emprator15.login("");
+emprator15.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////
+emprator16.login("");
+emprator16.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////
+emprator17.login("");
+emprator17.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////
+emprator18.login("");
+emprator18.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+///////
+emprator19.login("");
+emprator19.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+////////
+emprator20.login("");
+emprator20.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+////////
+emprator21.login("");
+emprator21.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+////////
+emprator22.login("");
+emprator22.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+////////
+emprator23.login("");
+emprator23.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+////////
+emprator24.login("");
+emprator24.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+////////
+emprator25.login("");
+emprator25.on("message", message => {
+  if (message.channel.id === "NONE") {
+    message.react("🤍").then(() => {
+      message.react("🖤");
+    });
+  }
+});
+//////////////////
